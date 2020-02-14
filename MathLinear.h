c27594585dee737d01809951e5256ab5e05a482f
batabek <atabek.berk@gmail.com>
2020-02-14 12:54:20 +0100

    Fix commenting

--- MathLinear.h
+++ MathLinear.h
@@ -19,16 +19,16 @@
 //
 //                [Classes]
 //                vec2d    : 2D vector data structure
-//				        vec3d    : 3D vector data structure
-//				        vec4d    : 4D vector data structure
+//				  vec3d    : 3D vector data structure
+//				  vec4d    : 4D vector data structure
 //                mat3x3   : 3x3 Matrix
 //                mat4x4   : 4x4 Matrix
 //                ray      : Parametric line segment
 //                plane    : 3D plane structure
-//				        rectangle: 2D bounding rectangle
-//				        AABB     : Axis-Aligned-Bounding-Box
+//				  rectangle: 2D bounding rectangle
+//				  AABB     : Axis-Aligned-Bounding-Box
 //                frustum  : view frustum for visibility testing
-//				        colorRGB : RGB triplet color structure
+//				  colorRGB : RGB triplet color structure
 //
 //  Note        : All matrix transformations are right handed and row major.
 //                In order to use them with OpenGL pipeline users need to use
